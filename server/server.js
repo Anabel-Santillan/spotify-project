4ddf2b77f0bc7aabc9c0c69647b6a69696b333ca
Anabel-Santillan <anabels.20022@gmail.com>
2023-08-22 20:35:50 -0400

    tried to address invalid client error.. But no luck so far

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config() 
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
 const bodyParser = require("body-parser")
@@ -15,20 +16,16 @@ app.post("/login", (req, res) => {
     clientSecret: process.env.REACT_APP_CLIENT_SECRET,
   });
 
-  spotifyApi.authorizationCodeGrant(code).then(
-    function(data) {
-      console.log('The token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
-      console.log('The refresh token is ' + data.body['refresh_token']);
-  
-      // Set the access token on the API object to use it in later calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-      spotifyApi.setRefreshToken(data.body['refresh_token']);
-    },
-    function(err) {
-      console.log('Something went wrong!', err);
-    }
-  );
+  spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err.message);
+  });
 });
 
 app.listen(3001)
